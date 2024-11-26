62e8c0dbe621075ae39fe7e2364ff82e123f18b1
Sylas Qiu <qiusy@oocl.com>
2024-11-26 16:12:22 +0800

    [SpringDataJpa] feat: change database repository for get employee by id

--- src/main/java/com/oocl/springbootemployee/service/EmployeeService.java
+++ src/main/java/com/oocl/springbootemployee/service/EmployeeService.java
@@ -37,7 +37,7 @@ public class EmployeeService {
     }
 
     public Employee findById(Integer employeeId) {
-        return employeeInMemoryRepository.findById(employeeId);
+        return employeeRepository.findById(employeeId).orElse(null);
     }
 
     public Employee create(Employee employee) {
